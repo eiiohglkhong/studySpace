ff193717d602b0cf133695055f763a80a34b9c32
eiiohglkhong <searcher.hong@gmail.com>
2015-07-02 18:16:16 +0800

    pom change

---
+++ src/main/java/com/java/test/dao/BaseDao.java
@@ -0,0 +1,8 @@
+package com.java.test.dao;
+
+/**
+ * Created by Kevin on 2015/7/2.
+ */
+public interface BaseDao {
+    public void init();
+}
---
+++ src/main/java/com/java/test/dao/impl/BaseDaoImpl.java
@@ -0,0 +1,12 @@
+package com.java.test.dao.impl;
+
+import com.java.test.dao.BaseDao;
+
+/**
+ * Created by Kevin on 2015/7/2.
+ */
+public class BaseDaoImpl implements BaseDao {
+    public void init() {
+
+    }
+}
